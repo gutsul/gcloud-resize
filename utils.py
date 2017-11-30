d293ce21b03cf691905a9ce3c98564b93be088d3
Grigortsevich Yuriy <GrigortsevichYuriy@gmail.com>
2017-11-30 11:32:05 +0200

    Added get size in mb.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from models import Disk
@@ -19,3 +20,13 @@ def parse_disks(json):
     disks.append(Disk(name=name, index=index))
 
   return disks
+
+
+def get_size_mb(start_path='.'):
+  total_size = 0
+  for dirpath, dirnames, filenames in os.walk(start_path):
+    for f in filenames:
+      fp = os.path.join(dirpath, f)
+      total_size += os.path.getsize(fp)
+
+  return total_size / 1000000
