bbee72e0862f8edf14282fb146776f8411ca20b2
gutsul27 <GtigortsevichYuriy@gmail.com>
2017-12-18 16:40:29 +0200

    Added new syslog messages.

--- gcloud-resize.py
+++ gcloud-resize.py
@@ -42,6 +42,10 @@ def resize(ZONE, disk):
   new_size_gb = disk.size + add_gb
   api.resize_disk(name=disk.name, size_gb=new_size_gb, zone=ZONE)
 
+  msg = 'DEBUG ACTION="Resize disk." NAME="{}" ADD_GB={} NEW_SIZE_GB={}' \
+      .format(disk.name, add_gb, new_size_gb)
+  log(msg)
+
 
 def apply(disk):
   if disk.fstype == EXT4:
@@ -49,7 +53,13 @@ def apply(disk):
   elif disk.fstype == XFS:
     shell.resize_xfs_disk(label=disk.get_label)
   else:
-    print("Cannot resize. Not supported file system.")
+    msg = 'ERROR ACTION="Apply disk." NAME="{}" SOURCE="/dev/{}" FSTYPE="{}" REASON="Not supported file system."' \
+        .format(disk.name, disk.get_label(), disk.fstype)
+    log(msg)
+
+  msg = 'DEBUG ACTION="Apply disk." NAME="{}" SOURCE="/dev/{}" FSTYPE="{}"' \
+      .format(disk.name, disk.get_label(), disk.fstype)
+  log(msg)
 
 
 def main():
