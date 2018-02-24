75299219e7c9089bb1201f4fdf9e7344c699cfee
gutsul <GrigortsevichYuriy@gmail.com>
2018-02-24 15:10:48 +0200

    clean settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Customized settings
 
 # GCload Project ID
-PROJECT_ID = 'adlithium-1289'
+PROJECT_ID = ''
 
 # Resize disk when free disk space < <FREE_LIMIT_PERCENT>
 FREE_LIMIT_PERCENT = 10
@@ -9,9 +9,8 @@ FREE_LIMIT_PERCENT = 10
 # Resize disk space up to <RESIZE_PERCENT>
 RESIZE_PERCENT = 20
 
-
 # Slack Integration
-SLACK_URL = 'https://hooks.slack.com/services/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'
+SLACK_URL = ''
 
 ## Send message to @nickname
-SLACK_USERS = '<@ygrigortsevich> <@victordementiev> <@alexander>'
+SLACK_USERS = '<@username1> <@username2>'
