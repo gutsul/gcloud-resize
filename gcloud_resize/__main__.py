aff848ac3c495ee280372014c3816e4dc8cefec8
gutsul <GrigortsevichYuriy@gmail.com>
2018-10-22 21:53:56 +0300

    Changed main entrypoint for test purposes only.

--- gcloud_resize/__main__.py
+++ gcloud_resize/__main__.py
@@ -1,27 +1,38 @@
-from gcloud_resize.utils import is_root
+# from gcloud_resize.utils import is_root
+#
+# if not is_root():
+#   exit("\nOnly root can run this script\n")
+#
+# from gcloud_resize import rest, integrations
+# from gcloud_resize.logger import info, debug
+#
+#
+# def main():
+#   instance = rest.InstanceDetails()
+#   slack = integrations.Slack(instance=instance)
+#
+#   info("Instance '{}' [{}]: Run on '{}' environment.".format(instance.name, instance.zone, instance.environment))
+#
+#   for disk in instance.disks:
+#
+#     if not disk.boot:
+#       debug("Disk '{}' [{}]: Disk is not boot.".format(disk.name, disk.device))
+#
+#       if disk.low():
+#         info("Disk '{}' [{}]: A disk has a low space. ".format(disk.name, disk.device))
+#         response = instance.send_request_to_resize(disk)
+#         instance.apply_changes(disk)
+#         slack.post(disk)
+#     else:
+#       debug("Disk '{}' [{}]: Disk is boot. Nothing to do.".format(disk.name, disk.device))
 
-if not is_root():
-  exit("\nOnly root can run this script\n")
+from gcloud_resize.models import Disk
 
-from gcloud_resize import rest, integrations
-from gcloud_resize.logger import info, debug
+disk = Disk(name="test", index=0, boot=True)
 
-
-def main():
-  instance = rest.InstanceDetails()
-  slack = integrations.Slack(instance=instance)
-
-  info("Instance '{}' [{}]: Run on '{}' environment.".format(instance.name, instance.zone, instance.environment))
-
-  for disk in instance.disks:
-
-    if not disk.boot:
-      debug("Disk '{}' [{}]: Disk is not boot.".format(disk.name, disk.label))
-
-      if disk.low():
-        info("Disk '{}' [{}]: A disk has a low space. ".format(disk.name, disk.label))
-        response = instance.send_request_to_resize(disk)
-        instance.apply_changes(disk)
-        slack.post(disk)
-    else:
-      debug("Disk '{}' [{}]: Disk is boot. Nothing to do.".format(disk.name, disk.label))
+print("---")
+print("device: {}".format(disk.device))
+print("fstype: {}".format(disk.fstype))
+print("total: {}".format(disk.total))
+print("free: {}".format(disk.free))
+print("percent: {}".format(disk.percent))
